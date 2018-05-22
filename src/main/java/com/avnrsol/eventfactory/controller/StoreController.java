21837b58029e7e796f8c0379d04477b62c26296e
unknown <erfanjordison@gmail.com>
2018-05-23 04:22:34 +0600

    working

--- src/main/java/com/avnrsol/eventfactory/controller/StoreController.java
+++ src/main/java/com/avnrsol/eventfactory/controller/StoreController.java
@@ -1,9 +1,12 @@
 package com.avnrsol.eventfactory.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.avnrsol.eventfactory.Repository.ServiceCategoryRepository;
@@ -36,6 +39,15 @@ public class StoreController {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("home");
 		 modelAndView.addObject("clientlist", serviceCategoryRepository.findAll());
+		return modelAndView;
+	}
+	
+	
+	@RequestMapping(value="/service", method = RequestMethod.GET)
+	public ModelAndView service(@RequestParam("id") Long id){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("single-product-sidebar");
+		modelAndView.addObject("serv", serviceoRepository.findServiceoById(id));
 		return modelAndView;
 	}
 	
