2e37416a89fcf28f598d49b60910c2d3697c09c1
System Administrator <root@Tasnims-MacBook-Pro.local>
2018-05-24 13:55:31 +0600

    single service view page

--- src/main/java/com/avnrsol/eventfactory/controller/StoreController.java
+++ src/main/java/com/avnrsol/eventfactory/controller/StoreController.java
@@ -1,7 +1,5 @@
 package com.avnrsol.eventfactory.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,45 +9,40 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.avnrsol.eventfactory.Repository.ServiceCategoryRepository;
 import com.avnrsol.eventfactory.Repository.ServiceoRepository;
-import com.avnrsol.eventfactory.Repository.VendorRepository;
-import com.avnrsol.eventfactory.service.ServiceCategoryService;
-import com.avnrsol.eventfactory.service.ServiceoService;
 import com.avnrsol.eventfactory.service.UserService;
 import com.avnrsol.eventfactory.service.VendorService;
 
 @Controller
 public class StoreController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private ServiceoRepository serviceoRepository;
-	
-	
+
 	@Autowired
 	private ServiceCategoryRepository serviceCategoryRepository;
-	
-	@Autowired
-	private VendorRepository vendorRepository;
-	
 
-	@RequestMapping(value="/", method = RequestMethod.GET)
-	public ModelAndView index(){
+	@Autowired
+	private VendorService vendorService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("home");
-		 modelAndView.addObject("clientlist", serviceCategoryRepository.findAll());
+		modelAndView.addObject("clientlist", serviceCategoryRepository.findAll());
+		modelAndView.addObject("vendors", vendorService.findAllVendor(0));
+		
 		return modelAndView;
 	}
-	
-	
-	@RequestMapping(value="/service", method = RequestMethod.GET)
-	public ModelAndView service(@RequestParam("id") Long id){
+
+	@RequestMapping(value = "/service", method = RequestMethod.GET)
+	public ModelAndView service(@RequestParam("id") Long id) {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("single-product-sidebar");
 		modelAndView.addObject("serv", serviceoRepository.findServiceoById(id));
 		return modelAndView;
 	}
-	
-		
+
 }
